bb036007a5425036e5708e920ee9fca65e0d2ac0
meetk-yi <meet.k@york.ie>
2025-09-02 13:44:57 +0530

    initial commit

---
+++ src/ZipSync.tsx
@@ -0,0 +1,13 @@
+function ZipSync() {
+  return (
+    <div className="flex min-h-screen items-center justify-center bg-gray-100">
+      <div className="rounded-2xl bg-white p-8 shadow-lg">
+        <h1 className="text-4xl font-bold text-blue-600 mb-4">
+          ZipSync
+        </h1>
+      </div>
+    </div>
+  )
+}
+
+export default ZipSync
---
+++ src/main.tsx
@@ -0,0 +1,5 @@
+import { createRoot } from "react-dom/client";
+import ZipSync from "./ZipSync.tsx";
+import "./index.css";
+
+createRoot(document.getElementById("root")!).render(<ZipSync />);
